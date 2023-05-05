6c9925421fc778da230f1fc1d4421a016757beab
gokarna123-goku <agokarna444@gmail.com>
2023-05-05 19:43:27 +0545

    added javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+//
+
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector("nav");
+  var scrollY = window.scrollY;
+
+  if (scrollY >= 50) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
+
+// End
